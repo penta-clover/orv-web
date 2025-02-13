8c4250a43629b0c48e8f41b4df065b43640d2720
CaChiJ <chj7239@gmail.com>
2025-02-13 14:13:58 +0900

    init project

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Orv",
+  description: "나를 바라보는 시간",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
