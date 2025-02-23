a87670613b6ea10d6a923620038e6ad4fbf2a31f
CaChiJ <chj7239@gmail.com>
2025-02-23 22:41:55 +0900

    메타 태그 무시되는 문제 해결

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import "./globals.css";
-import Head from "next/head";
 
 import * as ChannelService from "@channel.io/channel-web-sdk-loader";
 import { SpeedInsights } from "@vercel/speed-insights/next";
@@ -17,15 +16,16 @@ export default function RootLayout({
 
   return (
     <html lang="ko">
-      <Head>
-        <title>나를 마주하는 시간 오브</title>
-        <meta name="description" content="나를 마주할 기회를 선물하는 곳" />
+      <title>나를 마주하는 시간 오브</title>
+      <meta name="description" content="나를 마주할 기회를 선물하는 곳" />
 
-        <meta property="og:title" content="오브 Orv" />
-        <meta property="og:description" content="나를 마주할 기회를 선물하는 곳" />
-        <meta property="og:image" content="/images/orv-og-thumbnail.jpeg" />
-        <meta property="og:url" content="www.orv.im" />
-      </Head>
+      <meta property="og:title" content="오브 Orv" />
+      <meta
+        property="og:description"
+        content="나를 마주할 기회를 선물하는 곳"
+      />
+      <meta property="og:image" content="/images/orv-og-thumbnail.jpeg" />
+      <meta property="og:url" content="www.orv.im" />
       <body className={`antialiased hide-scrollbar safe-area font-pretendard`}>
         <Analytics />
         <SpeedInsights />
