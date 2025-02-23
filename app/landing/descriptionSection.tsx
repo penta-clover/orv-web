a6b5e91e9aab8b649782f1a2b4c39ca4bc224f30
CaChiJ <chj7239@gmail.com>
2025-02-23 16:45:34 +0900

    description 색상 변경

--- app/landing/descriptionSection.tsx
+++ app/landing/descriptionSection.tsx
@@ -27,7 +27,7 @@ export default function DescriptionSection() {
         });
       },
       {
-        rootMargin: "0px 0px -20% 0px",
+        rootMargin: "0px 0px -30% 0px",
       }
     );
 
@@ -56,14 +56,14 @@ export default function DescriptionSection() {
       >
         <div
           className={`font-medium text-[16px]/[26px] transition-colors duration-700 ${
-            textVisible[0] ? "text-gray-500" : "text-dark"
+            textVisible[0] ? "text-main-lilac50" : "text-dark"
           }`}
         >
           내가 하지 않으면 그 누구도 대신해주지 않을
         </div>
         <div
           className={`font-medium text-[16px]/[26px] transition-colors duration-700 ${
-            textVisible[0] ? "text-gray-500" : "text-dark"
+            textVisible[0] ? "text-main-lilac50" : "text-dark"
           }`}
         >
           "나 스스로에 대해 알아가기"
@@ -78,21 +78,21 @@ export default function DescriptionSection() {
       >
         <div
           className={`font-medium text-[16px]/[26px] transition-colors duration-700 ${
-            textVisible[1] ? "text-gray-500" : "text-dark"
+            textVisible[1] ? "text-main-lilac50" : "text-grayscale-500"
           }`}
         >
           누구나 한번쯤은 '나는 누구인가' '언제 행복을 느끼는가'
         </div>
         <div
           className={`font-medium text-[16px]/[26px] transition-colors duration-700 ${
-            textVisible[1] ? "text-gray-500" : "text-dark"
+            textVisible[1] ? "text-main-lilac50" : "text-grayscale-500"
           }`}
         >
           '내가 원하는 것은 무엇일까'라는 생각을 해보지만
         </div>
         <div
           className={`font-medium text-[16px]/[26px] transition-colors duration-700 ${
-            textVisible[1] ? "text-gray-500" : "text-dark"
+            textVisible[1] ? "text-main-lilac50" : "text-grayscale-500"
           }`}
         >
           결국 잠깐일 뿐 잊혀지게 됩니다.
@@ -107,14 +107,14 @@ export default function DescriptionSection() {
       >
         <div
           className={`font-medium text-[16px]/[26px] transition-colors duration-700 ${
-            textVisible[2] ? "text-gray-500" : "text-dark"
+            textVisible[2] ? "text-main-lilac50" : "text-grayscale-500"
           }`}
         >
           오브에서는 당신을 위한 특별한 질문을 던지고
         </div>
         <div
           className={`font-medium text-[16px]/[26px] transition-colors duration-700 ${
-            textVisible[2] ? "text-gray-500" : "text-dark"
+            textVisible[2] ? "text-main-lilac50" : "text-grayscale-500"
           }`}
         >
           이에 대답하는 당신의 모습을 영상으로 남겨요
@@ -126,7 +126,7 @@ export default function DescriptionSection() {
           textRefs.current[3] = el;
         }}
         className={`text-head2 mb-[32px] transition-colors duration-700 ${
-          textVisible[3] ? "text-main-lilac50" : "text-dark"
+          textVisible[3] ? "text-main-lilac50" : "text-grayscale-500"
         }`}
       >
         지금 시작하세요
