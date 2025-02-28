2bfd9fe0ee2471a641d76481dd772b24856729a3
CaChiJ <chj7239@gmail.com>
2025-02-28 21:04:04 +0900

    refactor: on/off smartlook tracking

--- app/analytics.tsx
+++ app/analytics.tsx
@@ -2,6 +2,7 @@
 
 import * as amplitude from "@amplitude/analytics-browser";
 import Script from "next/script";
+import React from "react";
 import { useEffect } from "react";
 
 export default function Analytics() {
@@ -16,21 +17,45 @@ export default function Analytics() {
     }
   }, []);
 
-  return process.env.NEXT_PUBLIC_GA_TRACKING_ENABLED === "true" ? (
-    // google analytics
-    <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="gtag-init" strategy="afterInteractive">
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){ dataLayer.push(arguments); }
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-        `}
+  const scripts = [];
+
+  if (process.env.NEXT_PUBLIC_GA_TRACKING_ENABLED === "true") {
+    scripts.push(
+      // google analytics
+      <>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){ dataLayer.push(arguments); }
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+          `}
+        </Script>
+      </>
+    );
+  }
+
+  if (process.env.NEXT_PUBLIC_SMARTLOOK_TRACKING_ENABLED === "true") {
+    scripts.push(
+      <Script type="text/javascript" strategy="afterInteractive">
+        {`window.smartlook||(function(d) {
+        var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+        var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+        c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+        })(document);
+        smartlook('init', '971a90886549384a73f2e6d776aed58357d080fa', { region: 'eu' });
+      `}
       </Script>
-    </>
-  ) : null;
+    );
+  }
+
+  return <>{
+    scripts.map((script, index) => (
+      <React.Fragment key={index}>{script}</React.Fragment>
+    ))
+  }</>
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -17,19 +17,22 @@ export default function RootLayout({
 
   return (
     <html lang="ko">
-      <title>나를 마주하는 시간 오브</title>
-      <meta name="description" content="나를 마주할 기회를 선물하는 곳" />
+      <head>
+        <title>나를 마주하는 시간 오브</title>
+        <meta name="description" content="나를 마주할 기회를 선물하는 곳" />
 
-      <meta property="og:title" content="오브 Orv" />
-      <meta
-        property="og:description"
-        content="나를 마주할 기회를 선물하는 곳"
-      />
-      <meta property="og:image" content="/images/orv-og-thumbnail.jpeg" />
-      <meta property="og:url" content="www.orv.im" />
+        <meta property="og:title" content="오브 Orv" />
+        <meta
+          property="og:description"
+          content="나를 마주할 기회를 선물하는 곳"
+        />
+        <meta property="og:image" content="/images/orv-og-thumbnail.jpeg" />
+        <meta property="og:url" content="www.orv.im" />
+      </head>
       <body className={`antialiased hide-scrollbar safe-area font-pretendard`}>
         <Analytics />
         <SpeedInsights />
+
         <link
           rel="stylesheet"
           as="style"
@@ -38,16 +41,6 @@ export default function RootLayout({
         />
 
         <Providers>{children}</Providers>
-
-        <Script type="text/javascript" strategy="afterInteractive">
-          {`window.smartlook||(function(d) {
-            var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
-            var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
-            c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
-            })(document);
-            smartlook('init', '971a90886549384a73f2e6d776aed58357d080fa', { region: 'eu' });
-          `}
-        </Script>
       </body>
     </html>
   );
