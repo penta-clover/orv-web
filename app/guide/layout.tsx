4761a25612cd99ad9f77ba02d1812d6d69b11358
CaChiJ <chj7239@gmail.com>
2025-02-28 20:38:52 +0900

    fix: 사용가이드 스크롤 버그 해결

--- app/guide/layout.tsx
+++ app/guide/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({
 }>) {
 
   return (
-    <div className="flex flex-row w-full overflow-x-hidden">
+    <div className="flex flex-row w-full overflow-x-hidden overflow-y-hidden">
       <div className="flex grow bg-[#050505]" />
       <div className="relative w-[100dvw] max-w-[450px]">{children}</div>
       <div className="flex grow bg-[#050505]" />
