ed201f58ec1b3a60c0dce8ebe4594ee1ba7af303
CaChiJ <chj7239@gmail.com>
2025-03-01 14:05:46 +0900

    필터 추가

--- app/trial/page.tsx
+++ app/trial/page.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 
 export default function Page() {
   const [currentQuestionIdx, setCurrentQuestionIdx] = useState<number>(0);
+  const [filter, setFilter] = useState<string>("");
 
   const questions = [
     "나를 설명하는 한 단어는 무엇인가요?",
@@ -20,11 +21,17 @@ export default function Page() {
 
   return (
     <div className="relative w-full h-[100dvh] flex flex-col justify-center">
-      <div className="relative inset-y-0 max-h-[100dvh] overflow-y-hidden">
-        <div>
-          <Image src="/images/studio-lighting.png" fill alt="studio-lighting" />
+      <div className="relative inset-y-0 max-h-[100dvh] lg:max-h-[90dvh] overflow-y-hidden">
+        <div className="inset-y-0" style={{ filter: filter || "none" }}>
+          <div>
+            <Image
+              src="/images/studio-lighting.png"
+              fill
+              alt="studio-lighting"
+            />
+          </div>
+          <CameraComponent />
         </div>
-        <CameraComponent />
         <div className="absolute top-[0] z-50 w-full h-full flex flex-col justify-end items-start">
           {/* <div className="bg-grayscale-black text-grayscale-white text-head3 px-[10px] py-[5px] flex justify-center items-center">
             {`Q${currentQuestionIdx + 1}. ${questions[currentQuestionIdx]}`}
@@ -34,22 +41,58 @@ export default function Page() {
             다섯번째 질문
           </div>
           <div className="font-light text-body1 leading-[20px] lg:text-[44px] lg:leading-[54px] text-grayscale-white text-center ml-[20px] mr-[20px] lg:ml-[72px] text-start">
-            살아오며 가장 행복했던 순간은 언제인가요?
+            {questions[currentQuestionIdx]}
           </div>
           <div className="h-[2vh] lg:h-[70px]" />
         </div>
       </div>
 
-      {/* <div className="absolute bottom-[50px] flex flex-row w-full justify-end">
+      <div className="absolute bottom-[50px] flex flex-row w-full justify-end">
+        <button
+          className="bg-grayscale-100 rounded px-[10px] py-[10px]"
+          onClick={() => setFilter("")}
+        >
+          원본
+        </button>
+        <button
+          className="bg-grayscale-100 rounded px-[10px] py-[10px]"
+          onClick={() => setFilter("sepia(100%)")}
+        >
+          갈색 필터
+        </button>
+        <button
+          className="bg-grayscale-100 rounded px-[10px] py-[10px]"
+          onClick={() => setFilter("grayscale(100%)")}
+        >
+          흑백 필터
+        </button>
+        <button
+          className="bg-grayscale-100 rounded px-[10px] py-[10px]"
+          onClick={() => setFilter("invert(100%)")}
+        >
+          반전 필터
+        </button>
+        <button
+          className="bg-grayscale-100 rounded px-[10px] py-[10px]"
+          onClick={() => setFilter("blur(3px)")}
+        >
+          블러 필터
+        </button>
+        <button
+          className="bg-grayscale-100 rounded px-[10px] py-[10px]"
+          onClick={() => setFilter("contrast(200%)")}
+        >
+          높은 대비 필터
+        </button>
         <button
           className="bg-grayscale-100 rounded px-[10px] py-[10px]"
           onClick={() =>
             setCurrentQuestionIdx((prev) => (prev + 1) % questions.length)
           }
         >
-          다음으로
+          다음 질문
         </button>
-      </div> */}
+      </div>
     </div>
   );
 }
@@ -58,11 +101,10 @@ function CameraComponent() {
   const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
-    // 브라우저 환경인지 확인
+    // 브라우저 환경인지 확인 후 카메라 접근
     if (typeof window !== "undefined" && navigator.mediaDevices) {
       const enableCamera = async () => {
         try {
-          // 사용자에게 카메라 접근 권한 요청
           const stream = await navigator.mediaDevices.getUserMedia({
             video: true,
           });
@@ -83,8 +125,11 @@ function CameraComponent() {
       ref={videoRef}
       autoPlay
       playsInline
-      //   style={{ width: "100%", filter: "grayscale(100%)" }}
-      style={{ width: "100%", aspectRatio: "16/9", objectFit: "cover" }}
+      style={{
+        width: "100%",
+        aspectRatio: "16/9",
+        objectFit: "cover",
+      }}
     />
   );
 }
