95903daef6d189859a6171bc514910a01ba5d6be
CaChiJ <chj7239@gmail.com>
2025-03-06 16:10:00 +0900

    배경 추가

--- app/landing/v2/(components)/worrySection.tsx
+++ app/landing/v2/(components)/worrySection.tsx
@@ -30,26 +30,26 @@ export default function WorrySection() {
 
       <div className="flex flex-col items-center text-body4 text-grayscale-200 w-full max-w-[384px] gap-[22px] px-[16px]">
         <div className="flex flex-row w-full">
-          <span className={`bg-grayscale-700 rounded-[8px] p-[12px] transition-all ${scrolling ? "animate-shake-1 text-grayscale-800 bg-grayscale-400" : ""}`}>
+          <span className={`rounded-[8px] p-[12px] transition-all ${scrolling ? "animate-shake-1 text-grayscale-800 bg-grayscale-400" : "bg-grayscale-700"}`}>
             요즘 나를 가장 힘들게 하는 것은?
           </span>
           <span className="grow" />
         </div>
         <div className="flex flex-row w-full">
           <span className="grow" />
-          <span className={`bg-grayscale-800 rounded-[8px] p-[12px] transition-all ${scrolling ? "animate-shake-0.7 text-grayscale-800 bg-grayscale-200" : ""}`}>
+          <span className={`rounded-[8px] p-[12px] transition-all ${scrolling ? "animate-shake-0.7 text-grayscale-800 bg-grayscale-200" : "bg-grayscale-800"}`}>
             최근 행복하다고 느꼈던 상황은 언제였나요?
           </span>
         </div>
         <div className="flex flex-row w-full">
-          <span className={`bg-grayscale-700 rounded-[8px] p-[12px] transition-all ${scrolling ? "animate-shake-1.2 text-grayscale-800 bg-grayscale-400" : ""}`}>
+          <span className={`rounded-[8px] p-[12px] transition-all ${scrolling ? "animate-shake-1.2 text-grayscale-800 bg-grayscale-400" : "bg-grayscale-700"}`}>
             과거로 돌아갈 수 있다면 언제로 돌아가고 싶나요?
           </span>
           <span className="grow" />
         </div>
         <div className="flex flex-row w-full">
           <span className="grow" />
-          <span className={`bg-grayscale-800 rounded-[8px] p-[12px] transition-all ${scrolling ? "animate-shake-0.9 text-grayscale-800 bg-grayscale-200" : ""}`}>
+          <span className={`rounded-[8px] p-[12px] transition-all ${scrolling ? "animate-shake-0.9 text-grayscale-800 bg-grayscale-200" : "bg-grayscale-800"}`}>
             내일 죽는다면 오늘은 무엇을 하고 싶은가요?
           </span>
         </div>
