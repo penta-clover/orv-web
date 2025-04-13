3413b961b31e86b5d708c8ce52777e51b9f07916
CaChiJ <chj7239@gmail.com>
2025-04-13 19:48:41 +0900

    og - description 변경

--- app/events/invitation/InvitationPage.client.tsx
+++ app/events/invitation/InvitationPage.client.tsx
@@ -193,14 +193,14 @@ function topicMapper(topic: string): {
     HIDDEN_3fEMQ1: {
       text: "같은 주제의 질문 중 “유튜브를 운영하며 가장 힘들었던 점은 무엇인가요?”라는 질문에 “열심히 만든 영상이었는데 사람들이 반응이 나타나지 않을 때”라고 답변",
       ogTitle: "오브를 즐겨쓰는 친구가 초대했어요.",
-      ogDescription: "친구가 추천하는 주제로 참여가능해요.",
+      ogDescription: "노트북으로 입장해주세요.",
       ogImage:
         "http://d3bdjeyz3ry3pi.cloudfront.net/static/images/invitation-og.png",
     },
     HIDDEN_3fEMQ2: {
       text: "같은 주제의 질문 중 “왜 HySpark에 들어 오려고 했나요?”라는 질문에 “언젠가 죽는 날이 다가왔을 때 그때 해볼 걸이라고 후회할 것 같아서 모든 것을 해보겠다는 마음으로 지원했습니다.”라고 답변",
       ogTitle: "오브를 즐겨쓰는 친구가 초대했어요.",
-      ogDescription: "친구가 추천하는 주제로 참여가능해요.",
+      ogDescription: "노트북으로 입장해주세요.",
       ogImage:
         "http://d3bdjeyz3ry3pi.cloudfront.net/static/images/invitation-og.png",
     },
