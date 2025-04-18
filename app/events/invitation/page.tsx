f156bac087062c4a92dda3b06ecbf5091d109c29
CaChiJ <chj7239@gmail.com>
2025-04-18 13:02:59 +0900

    feat: 친구 초대 링크 추가

--- app/events/invitation/page.tsx
+++ app/events/invitation/page.tsx
@@ -47,6 +47,20 @@ function topicMapper(topic: string): {
       ogImage:
         "http://d3bdjeyz3ry3pi.cloudfront.net/static/images/invitation-og.png",
     },
+    HIDDEN_3fEMQ3: {
+      text: "같은 주제의 질문 중 “이번 생일에 가장 고마움을 느끼는 상대가 있나요?”라는 질문에 “축하해준 사람들 모두한테 정말 고맙다고 느끼지만, 그 중에서도 예상치 못하게 오랜만에 연락을 준 친구들이 특히 더 고마웠던 것 같아요. 생일이라고 하더라도 연락을 안하다가 갑자기 연락하는 일은 쉬운 일이 아니라고 생각해서요”라고 답변",
+      ogTitle: "오브를 즐겨쓰는 친구가 초대했어요.",
+      ogDescription: "노트북으로 입장해주세요.",
+      ogImage:
+        "http://d3bdjeyz3ry3pi.cloudfront.net/static/images/invitation-og.png",
+    },
+    HIDDEN_3fEMQ4: {
+      text: "같은 주제의 질문 중 “만약 주변 사람들의 시선이나 경제적인 제약이 없다면 당장 무엇을 할 건가요?”라는 질문에 “명품차까지는 아니더라도 차 사고 집도 좋은 곳으로 사고 싶어요. 노숙인들도 돕고 싶고.. 가치있는 일을 하고 싶어요.”라고 답변",
+      ogTitle: "오브를 즐겨쓰는 친구가 초대했어요.",
+      ogDescription: "노트북으로 입장해주세요.",
+      ogImage:
+        "http://d3bdjeyz3ry3pi.cloudfront.net/static/images/invitation-og.png",
+    },
   };
 
   return data[topic];
