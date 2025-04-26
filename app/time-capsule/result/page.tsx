ae779006b21ed14324e1284f221a23a623e8e083
CaChiJ <chj7239@gmail.com>
2025-04-26 20:27:35 +0900

    다운로드 해상도 개선

--- app/time-capsule/result/page.tsx
+++ app/time-capsule/result/page.tsx
@@ -554,7 +554,7 @@ const handleCapture = async (elem: HTMLElement) => {
     const canvas = await html2canvas(elem, {
       useCORS: true,
       backgroundColor: "rgb(16, 16, 18)",
-      scale: 2,
+      scale: window.devicePixelRatio * 2,
     });
 
     canvas.toBlob((blob) => {
